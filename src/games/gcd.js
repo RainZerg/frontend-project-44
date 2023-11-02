59b036e5e02b2cacf465aa4c0a9ae0e9ae0061f3
root <root@DESKTOP-H4G38O8>
2023-11-02 14:39:01 +0300

    fixed r to n in line 4

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,7 +1,7 @@
 import { gameEngine } from '../gameEngine.js';
 import { getRandomNum } from '../getRandomNumber.js';
 
-const rule = 'Find the gratest common divisor of giver numbers.';
+const rule = 'Find the gratest common divisor of given numbers.';
 
 function answerGcd(a, b) {
     if (!b) {
