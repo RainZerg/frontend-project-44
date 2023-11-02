25194846f7e1c164aca1ba8bbc8061657df87ca0
root <root@DESKTOP-H4G38O8>
2023-11-02 14:47:45 +0300

    fixed typos

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,7 +1,7 @@
 import { gameEngine } from '../gameEngine.js';
 import { getRandomNum } from '../getRandomNumber.js';
 
-const rule = 'Find the gratest common divisor of given numbers.';
+const rule = 'Find the greatest common divisor of given numbers.';
 
 function answerGcd(a, b) {
     if (!b) {
